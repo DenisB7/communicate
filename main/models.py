19ccc836821667ff343f2ead750c61e8d67c75d6
DenisB7 <denzelbe1987@gmail.com>
2023-01-28 11:34:05 +0500

    Create tables

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_teacher = models.BooleanField(default=False)
+    teacher_online = models.IntegerField(default=0)
+    is_student = models.BooleanField(default=False)
+    student_online = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.user
+
+
+class MessagesUnread(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student')
+    student_unread_count = models.IntegerField()
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher')
+    teacher_unread_count = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"Student: {self.student.username}, Teacher: {self.teacher.username}"
