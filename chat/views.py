f60b280ebeda00d2e2307dfb63fffebfa59f90f9
DenisB7 <denzelbe1987@gmail.com>
2023-01-28 14:04:43 +0500

    Create chat page

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def room(request, room_name):
+    context = {
+        "user_is_authenticated": request.user.is_authenticated,
+        "room_name": room_name,
+    }
+    return render(request, "chat/room.html", context)
--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="main"),
-    path("login/", LoginView.as_view(template_name="templates/main/login.html"), name="login-user"),
-    path("logout/", LogoutView.as_view(), name="logout-user"),
+    path("login", LoginView.as_view(template_name="templates/main/login.html"), name="login-user"),
+    path("logout", LogoutView.as_view(), name="logout-user"),
 ]
